546a945f89c2fc1a47cc624e8cc84658bd5344b8
alex <naichuan@umich.edu>
2018-02-09 01:41:39 -0500

    add posts

--- coindigo_v0/coindigo_app/admin.py
+++ coindigo_v0/coindigo_app/admin.py
@@ -8,6 +8,18 @@ class UserInfoAdmin(admin.ModelAdmin):
         model = UserInfo
 
 
+class CategoryAdmin(admin.ModelAdmin):
+    list_display = ["__str__"]
+    class Meta:
+        model = Category
+
+class BlogAdmin(admin.ModelAdmin):
+    list_display = ["__str__", "get_owner", "get_title", "get_is_publish", "get_content", "get_created", "get_last_modified"]
+    class Meta:
+        model = Blog
+
 
 
 admin.site.register(UserInfo, UserInfoAdmin)
+admin.site.register(Category, CategoryAdmin)
+admin.site.register(Blog, BlogAdmin)
--- coindigo_v0/coindigo_app/models.py
+++ coindigo_v0/coindigo_app/models.py
@@ -1,5 +1,7 @@
 from django.db import models
 from django.contrib.auth.models import User
+
+import datetime
 # Create your models here.
 
 
@@ -16,3 +18,36 @@ class UserInfo(models.Model):
         return self.user.is_staff
     def date_joined(self):
         return self.user.date_joined
+
+
+class Category(models.Model):
+    """Scam of the Week, ..."""
+    name = models.TextField()
+    def __str__(self):
+        return str(self.name)
+
+
+class Blog(models.Model):
+    owner = models.ForeignKey(User, blank=True, null=True, on_delete=True)
+    title = models.TextField(default = "Untitled",blank = False, null = False)
+    content = models.TextField(blank = True, null = True)
+    category = models.ForeignKey(Category, blank = True, null = True, on_delete=False)
+    blog_image = models.ImageField(blank = True, null = True, upload_to = "static/media")
+    created = models.DateTimeField(default= datetime.datetime.now())
+    is_publish = models.BooleanField(default= False, blank = False, null = False)
+    last_modified = models.DateTimeField(default= datetime.datetime.now())
+
+    def __str__(self):
+        return str(self.category)
+    def get_title(self):
+        return str(self.title)
+    def get_is_publish(self):
+        return str(self.is_publish)
+    def get_content(self):
+        return self.content
+    def get_owner(self):
+        return self.owner
+    def get_created(self):
+        return str(self.created)
+    def get_last_modified(self):
+        return str(self.last_modified)
