05d9db29f10501971050aaae7007368a3d0207d2
alex <naichuan@umich.edu>
2018-02-08 23:16:27 -0500

    starters

--- coindigo_v0/coindigo_app/admin.py
+++ coindigo_v0/coindigo_app/admin.py
@@ -1,3 +1,13 @@
 from django.contrib import admin
+from .models import *
 
 # Register your models here.
+class UserInfoAdmin(admin.ModelAdmin):
+    list_display = ["__str__", "email", "is_admin", "date_joined"]
+    class Meta:
+        model = UserInfo
+
+
+
+
+admin.site.register(UserInfo, UserInfoAdmin)
--- coindigo_v0/coindigo_app/models.py
+++ coindigo_v0/coindigo_app/models.py
@@ -1,3 +1,18 @@
 from django.db import models
-
+from django.contrib.auth.models import User
 # Create your models here.
+
+
+class UserInfo(models.Model):
+    user = models.OneToOneField(User, related_name='userinfo', on_delete = models.CASCADE)
+    user_image = models.ImageField(blank = True, null = True, upload_to = "static/media")
+    about = models.TextField(blank = True, null = True)
+    contact_info = models.TextField(blank = True, null = True)
+    def __str__(self):
+        return str(self.user)
+    def email(self):
+        return self.user.email
+    def is_admin(self):
+        return self.user.is_staff
+    def date_joined(self):
+        return self.user.date_joined
