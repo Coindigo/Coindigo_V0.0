c2610e40ae36302629e533473d6dcbf298c64a02
andrewlloyd04 <lloydas@umich.edu>
2018-02-11 23:54:34 -0500

    update setup

---
+++ coindigo_v0/setup.py
@@ -0,0 +1,22 @@
+
+from setuptools import setup
+
+setup(
+    name='coindigo',
+    version='0.1.0',
+    packages=['coindigo'],
+    include_package_data=True,
+    install_requires=[
+        'Flask==0.12.2',
+        'html5validator==0.2.8',
+        'pycodestyle==2.3.1',
+        'pydocstyle==2.0.0',
+        'pylint==1.7.4',
+        'nodeenv==1.2.0',
+        'sh==1.12.14',
+        'Flask-Testing==0.6.2',
+        'selenium==3.6.0',
+        'requests==2.18.4',
+        'arrow==0.10.0'
+    ],
+)
